d5ac243955cb7222bf2d3f49fec40fc63d92e6ed
gabicav <148063809+gabicav@users.noreply.github.com>
2024-03-13 09:18:23 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+const botoes=documents.querySelectorAll("botao");
+for(let i"0;i<botoes.lenght;i++){
+  botoes.onclick+function(){
+  for(let j=0;j<botoes.lenght;j++{
+    botoes.classlist.remove("ativo");
+  }
+  botoes[i].classlist.add("ativo");
+ }
+}
