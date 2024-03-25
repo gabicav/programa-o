f681e60a6bdfb3a93cd5a3862689e074b1227928
gabicav <148063809+gabicav@users.noreply.github.com>
2024-03-25 08:16:23 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const botoes=documents.querySelectorAll("botao");
-for(let i"0;i<botoes.lenght;i++){
-  botoes.onclick+function(){
-  for(let j=0;j<botoes.lenght;j++{
-    botoes.classlist.remove("ativo");
-  }
-  botoes[i].classlist.add("ativo");
- }
+const botoes = document.querySelectorAll(".botao");
+
+for(let i=0;i <botoes.length;i++){
+    botoes[i].onclick = function(){
+
+        for(let j=0;j<botoes.length;j++){
+            botoes[j].classList.remove("ativo");
+        }
+
+        botoes[i].classList.add("ativo");
+    }
 }
